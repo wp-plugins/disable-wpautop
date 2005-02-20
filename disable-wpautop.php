c5e02a7a5ef377798258fe25a65a624d3598c0de
MtDewVirus <MtDewVirus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-02-20 15:56:41 +0000

    Updating Plugin URI
    git-svn-id: https://plugins.svn.wordpress.org/disable-wpautop/trunk@790 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- disable-wpautop.php
+++ disable-wpautop.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Disable WPAUTOP
 Version: 1.0
-Plugin URI: http://dev.wp-plugins.org/browser/disable-wpautop/
+Plugin URI: http://mtdewvirus.com/code/
 Description: Disables the wpautop function for the_content and the_excerpt.
 Author: Nick Momrik
 Author URI: http://mtdewvirus.com/
